b6d43586d0531b7bcc9ef6689403eb40cc8c7ea5
ZeroSum24 <stephen_waddell@hotmail.co.uk>
2020-03-17 18:19:23 +0000

    Pooling varient fixed

--- models/networks.py
+++ models/networks.py
@@ -858,7 +858,7 @@ class RelationalNLayerDiscriminator(nn.Module):
         cnn_feats = self.model.forward(x)
 
         # pool and squeeze the model into the [2, 256] shape
-        cnn_feats = F.adaptive_avg_pool3d(cnn_feats, (256, 1, 1))
+        cnn_feats = F.adaptive_avg_pool2d(cnn_feats, cnn_feats.shape[-1])
         cnn_feats = cnn_feats.squeeze()
 
         # extract the relational features from the model
